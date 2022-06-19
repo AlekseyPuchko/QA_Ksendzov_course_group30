cbf0d757ef41126736d99b488bee9824a1d8a556
AlekseyPuchko <aleshalinguini@gmail.com>
2022-06-19 21:44:57 +0300

    add script3.js, index.html files

---
+++ HW-Javascript/script3.js
@@ -0,0 +1,15 @@
+function checkAge(age_1) {
+    var age_1 = prompt('How old are you?', 17);
+    var age_2 = 18, age_3 = 61;
+    if (age_1 < age_2) {
+        alert("You don't have access cause your age is " + age_1 + "." + " It's less then " + age_2)
+    }
+    else if (age_1 >= age_2, age_1 < age_3) {
+        alert("You are welcome!")
+    }
+    else if (age_1 > age_3) {
+        alert("Keep calm and watch Culture channel")
+    }
+    return age_1
+}
+checkAge()
