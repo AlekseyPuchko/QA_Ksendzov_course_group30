07277b3f3626e5ff4b4bfa1284d01a4d00db3bb9
Aleksey Puchko <41000691+AlekseyPuchko@users.noreply.github.com>
2022-06-19 22:30:26 +0300

    Update script3.js

--- HW-Javascript/script3.js
+++ HW-Javascript/script3.js
@@ -1,6 +1,6 @@
 function checkAge(age_1) {
-    var age_1 = prompt('How old are you?', 17);
-    var age_2 = 18, age_3 = 61;
+    let age_1 = +prompt('How old are you?', 17);
+    let age_2 = 18, age_3 = 61;
     if (age_1 < age_2) {
         alert("You don't have access cause your age is " + age_1 + "." + " It's less then " + age_2)
     }
